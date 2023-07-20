6c902fade01189804e7a1cad7caf0c2f55326586
kaya53 <jimin4661@gmail.com>
2023-07-20 19:00:43 +0900

    feat: publish main page

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -13,7 +13,7 @@ function Footer() {
     return (
         <div className="fixed-bottom container mx-auto px-0 w-auto h-10">
             <div className="row justify-content-between">
-                <a href="https://www.freepik.com/free-photo/colleagues-giving-fist-bump_3532664.htm#page=2&query=communication&position=23&from_view=search&track=sph" className='copyright text-decoration-none text-black pe-none col-6 mx-0 alignbottom'>Image by rawpixel.com on Freepik</a> 
+                <a href="https://www.freepik.com/free-photo/colleagues-giving-fist-bump_3532664.htm#page=2&query=communication&position=23&from_view=search&track=sph" className='copyright text-decoration-none text-black pe-none col-6 mx-0 align-bottom'>Image by rawpixel.com on Freepik</a> 
                 <div className="col-6 mx-0 text-end">
                     {linkData.map((data:link, idx:number) => (
                         <a href={data.linkStr} key={idx}>
